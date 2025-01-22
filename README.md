8006bdf087e8fb53d7314d7d40d826e452377ea2
Maciej <maciek_gda@o2.pl>
2025-01-22 14:09:06 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Zephyr F411RE demo project
+
+Simple [Zephyr RTOS](https://zephyrproject.org/) user app on GitHub Runners using UART/SPI/I2C interfaces:
+
+Board:
+* [Nucleo-F411RE](https://st.com/en/evaluation-tools/nucleo-f411re.html)
+
+Modules:
+
+* [GPS NEO-6MV2](https://components101.com/modules/neo-6mv2-gps-module) - UART
+* [OLED SSD1306 128X64](https://components101.com/displays/oled-display-ssd1306) - I2C
+* [Bluetooth HC05 HC06](https://components101.com/wireless/hc-05-bluetooth-module) - I2C
+* [Ethernet ENC28J60](https://components101.com/modules/enc28j60-ethernet-module) - SPI
---
+++ app/src/main.c
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2025 Maciej
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+// Source:
+// https://gist.github.com/disposedtrolley/f0edbef0e65dbd7b56207e4ffc35c8d1
+// https://github.com/HexabitzPlatform/STM32-Zephyr/tree/main
+
+// I2C:
+// zephyr\samples\boards\st\i2c_timing\src\main.c
+
+// &usart1 {
+//   pinctrl-0 = <&usart1_tx_pb6 &usart1_rx_pb7>;
+//   pinctrl-names = "default";
+//   current-speed = <115200>;
+//   status = "okay";
+// };
+
+#include <stdio.h>
+#include <string.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/uart.h>
+#include <zephyr/kernel.h>
+
+#define SLEEP_TIME_MS 100
+
+#define LED0_NODE DT_ALIAS(led0)
+
+// static uint8_t rx_data;
+
+const struct device *uart = DEVICE_DT_GET(DT_NODELABEL(usart1));
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+char uart_buffer[] = "hello world!\n";
+
+void print_uart(char *buf, uint8_t msg_len) {
+  for (int i = 0; i < msg_len; i++) {
+    uart_poll_out(uart, buf[i]);
+  }
+}
+
+int main(void) {
+  int ret = 0;
+
+  if (!gpio_is_ready_dt(&led)) {
+    return 0;
+  }
+
+  ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+  if (ret < 0) {
+    return 0;
+  }
+
+  if (!device_is_ready(uart)) {
+    return 0;
+  }
+
+  while (1) {
+    gpio_pin_toggle_dt(&led);
+
+    // ret = uart_poll_in(uart, &rx_data);
+    // if (ret == 0) {
+    //   uart_poll_out(uart, rx_data);
+    //   // printk("Received data: %c\n", rx_data);
+    // }
+
+    print_uart(uart_buffer, strlen(uart_buffer));
+
+    k_msleep(SLEEP_TIME_MS);
+  }
+  return 0;
+}
